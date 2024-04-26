0fa4b28f375f785343ff90e7d43451fbaf615682
ThomasNagel <tmj.nagel@gmail.com>
2024-04-26 16:51:55 +0200

    compute violation percentages

--- .github/processMetrics.py
+++ .github/processMetrics.py
@@ -1,30 +1,61 @@
 import pandas as pd
+import json
 
-FILENAME = "metrics.csv"
+METRICS_FILE = "metrics.csv"
+CONFIG_FILE = ".github/metrics_config.json"
+
+OPERATORS = {
+    "=":  lambda a, b : a == b,
+    "<=": lambda a, b : a <= b,
+    "<":  lambda a, b : a < b,
+    ">=": lambda a, b : a >= b,
+    ">":  lambda a, b : a > b
+}
+
+
+def threshold(category: pd.DataFrame, metric: str, threshold: list[str, int]) -> float:
+    values = list()
+    for ind in category.index:
+        val = category[metric][ind]
+
+        if type(val) == str:
+            val = float(val.replace(',', '.'))
+        
+        values.append(val)
+
+    if len(values) == 0:
+        return 0.0
+    print(metric, values)
+    violations = 0
+    op, t = threshold
+    for val in values:
+        if not OPERATORS[op](val, t):
+            violations = violations + 1
+
+    return violations / len(values)
+
+def compute_percentages(categories: dict[str, pd.DataFrame], config: dict[str, dict]) -> dict[str, float]:
+    percentages = dict()
+
+    for key, val in config.items():
+        percentages[key] = threshold(categories[val['type']], val['metric'], val['threshold'])
+    
+    return percentages
 
 def main():
-    df = pd.read_csv(FILENAME)
+    df = pd.read_csv(METRICS_FILE)
 
     categories = {c:df.loc[df.Kind == c].dropna(axis=1) for c in df.Kind.unique()}
 
-    for c in categories:
-        print(categories[c])
+    with open(CONFIG_FILE, 'r') as f:
+        config = json.load(f)
     
-    for c in categories:
-        print(categories[c].columns.tolist())
-        print()
-    
-    print(categories['Class'].CountDeclMethod)
-    print(categories['Class'].CountClassBase)
-    print(categories['Class'].CountClassCoupled)
-    print(categories['Class'].CountClassCoupledModified)
-    print(categories['Module File'].CountDeclFunction)
-    print(categories['Module File'].CountLineCode)
-    print(categories['Module File'].CountLineCodeDecl)
-    print(categories['Module File'].CountLineCodeExe)
-    print(categories['Module File'].CountStmt)
-    print(categories['Module File'].CountStmtDecl)
-    print(categories['Module File'].CountStmtExe)
+    print(categories)
+    percentages = compute_percentages(categories, config)
 
+    print(percentages)
+
+    
+    
 if __name__ == "__main__":
     main()
