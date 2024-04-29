d27f0266b61a0deeb0878601939f19d6f1c4a68e
ThomasNagel <tmj.nagel@gmail.com>
2024-04-29 21:31:11 +0200

    testing different files

--- src/test.py
+++ src/test.py
@@ -1,1 +1,24 @@
-print('hello world!')
+import kaas
+
+t = kaas.Test()
+print(t.getA())
+
+class bruh():
+    t = kaas.Test()
+    
+    def __init__(self):
+        self.a = t.getA()
+    
+    def getT(self):
+        return self.a
+        
+class Bozo():
+    def __init__(self):
+        self.b = bruh()
+    
+    def getB(self):
+        return self.b
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
