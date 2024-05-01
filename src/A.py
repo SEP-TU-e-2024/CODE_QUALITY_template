9177105f537abbc34e57f805b4d74cef88ba8a7f
ThomasNagel <tmj.nagel@gmail.com>
2024-05-02 01:33:23 +0200

    dependency matrix test

---
+++ src/A.py
@@ -0,0 +1,8 @@
+from B import B
+
+class A():
+    def __init__(self):
+        b = B()
+    
+    def getB(self):
+        return self.b
---
+++ src/B.py
@@ -0,0 +1,8 @@
+from A import A
+
+class B():
+    def __init__(self):
+        a = A()
+    
+    def getA(self):
+        return self.a
