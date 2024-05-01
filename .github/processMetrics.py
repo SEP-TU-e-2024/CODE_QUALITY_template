2fe4a06fbb91ce00177110edece9ededdf29b528
ThomasNagel <tmj.nagel@gmail.com>
2024-05-02 01:01:27 +0200

    combine the metrics of understand and simian

--- .github/processMetrics.py
+++ .github/processMetrics.py
@@ -1,52 +1,17 @@
 import pandas as pd
 import json
 
+from processUnderstand import *
+from processSimian import *
+
 METRICS_FILE = "metrics.csv"
+SIMIAN_FILE = "simian-out.txt"
 CONFIG_FILE = ".github/metrics_config.json"
 
 # Colors
 AQUA = '\033[94;1m'
 TABLE_COL = ('\033[97;4m', '\033[37;4;2m')
 END_COL = '\033[0m'
-
-OPERATORS = {
-    "=":  lambda a, b : a == b,
-    "<=": lambda a, b : a <= b,
-    "<":  lambda a, b : a < b,
-    ">=": lambda a, b : a >= b,
-    ">":  lambda a, b : a > b
-}
-
-
-def threshold(categories: list[pd.DataFrame], metric: str, threshold: list[str, int]) -> float:
-    values = list()
-    for category in categories:
-        for ind in category.index:
-            val = category[metric][ind]
-
-            if type(val) == str:
-                val = float(val.replace(',', '.'))
-            
-            values.append(val)
-
-    if len(values) == 0:
-        return 0.0
-    print(metric, values)
-    violations = 0
-    op, t = threshold
-    for val in values:
-        if not OPERATORS[op](val, t):
-            violations = violations + 1
-
-    return violations / len(values)
-
-def compute_percentages(categories: dict[str, pd.DataFrame], config: dict[str, dict]) -> dict[str, float]:
-    percentages = dict()
-    for key, val in config.items():
-        dfs = [categories[t] for t in val['type'] if t in categories]
-        percentages[key] = threshold(dfs, val['metric'], val['threshold'])
-    
-    return percentages
 
 def percentage_to_rank(percentage: float) -> int:
     if percentage < 0.04:
@@ -91,13 +56,23 @@ def nice_print(dictionary):
 
 def main():
     df = pd.read_csv(METRICS_FILE)
-
     categories = {c:df.loc[df.Kind == c].dropna(axis=1) for c in df.Kind.unique()}
+
     with open(CONFIG_FILE, 'r') as f:
         config = json.load(f)
+        config_understand = config['understand']
+        config_simian = config['simian']
+        config_combined = {**config_understand, **config_simian}
+
+    with open(SIMIAN_FILE, 'r') as f:
+        duplications, total_lines = parse_output(f)
+        print("Duplications: ", duplications, "\tTotal lines: ", total_lines)
     
-    percentages = compute_percentages(categories, config)
-    ranks = compute_ranks(percentages, config)
+    percentages_understand = compute_percentages_understand(categories, config_understand)
+    percentages_simian = compute_percentages_simian(duplications, total_lines)
+
+    percentages = {**percentages_understand, **percentages_simian}
+    ranks = compute_ranks(percentages, config_combined)
     grade = compute_grade(ranks)
 
     print(f"{AQUA}Percentages{END_COL}")
