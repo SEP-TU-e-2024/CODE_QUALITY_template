f33faf89155173706fa19f6e966d96e7ab48c09a
ThomasNagel <tmj.nagel@gmail.com>
2024-06-09 20:51:41 +0200

    formatting

--- .github/processDependencies.py
+++ .github/processDependencies.py
@@ -22,7 +22,7 @@ def compute_percentages_dependencies(matrix, columns, total_num_files):
     for i in range(num_files):
         for j in range(num_files):
             if matrix[i][j] != 0 and matrix[j][i] != 0 and i != j:
-                print(f"Dependency violation found at position: ({i}, {j}) \t Files: {columns[i]}, {columns[j]}")
+                print(f"Dependency violation found at position: ({i}, {j}) \t Files:\n{columns[i]}\n{columns[j]}\n")
                 violations += 1
 
     return {"Cyclic dependencies": violations / (2 * total_num_files)}
